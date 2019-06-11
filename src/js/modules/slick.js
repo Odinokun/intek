7ab241af36909d392428666a92e690b1204b01ce
odinokun <odinokun@gmail.com>
2019-06-11 15:46:49 +0300

    add tab in top section

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -1,26 +1,25 @@
 module.exports = function() {
 
-  // begin Slick slider
-
+  // begin top-slider
   $('#index-top__slider').slick({
     dots: false,
     arrows: false,
     infinite: true,
     speed: 500,
     fade: true,
-    autoplay: true,
-    cssEase: 'linear'
+    // autoplay: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '#index-top__slider-nav'
   });
-
-  // $('.slick').slick({
-  //   dots: true,
-  //   infinite: true,
-  //   speed: 500,
-  //   fade: true,
-  //   autoplay: true,
-  //   cssEase: 'linear'
-  // });
-
-  // end Slick slider
+  $('#index-top__slider-nav').slick({
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    asNavFor: '#index-top__slider',
+    focusOnSelect: true,
+    variableWidth:true
+  });
+  // end top-slider
 
 };
