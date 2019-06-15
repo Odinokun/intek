d796bc9c8236c116689f446b96c94b0ee26831a2
odinokun <odinokun@gmail.com>
2019-06-15 15:35:34 +0300

    did service deal section

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -128,4 +128,41 @@ module.exports = function () {
   });
   // end reviews-slider
 
+  // begin reviews-slider
+  $('#service-deal__slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots: false,
+    infinite: false,
+    speed: 500,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 1021,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 761,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 577,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 377,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  // end reviews-slider
+
 };
